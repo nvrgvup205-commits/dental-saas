e8aaaea819b6146b1a61d98fa2bdb20dcc0f91f9
nvrgvup205-commits <nvrgvup205@gmail.com>
2026-05-25 05:32:35 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://cfbxoqkxibhztovhyxqn.supabase.co'
-const supabaseKey = 'sb_publishable_LZykDAeD6v3EaW0VaJkSdw_Hhm2L2vG'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmYnhvcWt4aWJoenRvdmh5eHFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMDM5NjAsImV4cCI6MjA5MzY3OTk2MH0.bi7fLHfJ01nqThYIAUyUY-F4vLiLZ8R3TvCqrRcJNNU'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
